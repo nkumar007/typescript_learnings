e3ef905cbd03f4196d0a63264bb063307a97496e
nkumar007 <naveen.mbuff98@gmail.com>
2023-04-18 14:43:51 +0530

    tuples and enums

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,18 @@
 "use strict";
-let a = 20;
-if (a > 10) {
-    a += 10;
-}
-console.log(a);
+// Tuples is a typescript type that is used to store fixed number of values
+// example: 1, Naveen
+let user = [1, "Naveen"];
+// Enums are a new type in Typescript which help us to group a series of constants( works similar to that exists in C# and Java)
+// instead of having constants like these..
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+var Size;
+(function (Size) {
+    Size[Size["small"] = 1] = "small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 4] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-let a: number = 20;
+// Tuples is a typescript type that is used to store fixed number of values
 
-if (a > 10) {
-  a += 10;
+// example: 1, Naveen
+
+let user: [number, string] = [1, "Naveen"];
+
+// Enums are a new type in Typescript which help us to group a series of constants( works similar to that exists in C# and Java)
+// instead of having constants like these..
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+enum Size {
+  small = 1,
+  Medium = 2,
+  Large = 4,
 }
-console.log(a);
+
+let mySize: Size = Size.Medium;
+
+console.log(mySize);
